4f3b3d375d200ad539d27087fb3dfe23a0776bc1
aumiaik7 <aumi.aik7@gmail.com>
2014-11-26 14:35:57 +0600

    Hello first commit

---
+++ src/mysqltest/MySQLTest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mysqltest;
+
+/**
+ *
+ * @author Brute Force
+ */
+public class MySQLTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
